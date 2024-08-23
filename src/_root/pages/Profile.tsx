a848a4d4eeefdfd73522ec5c61333bda46a6550f
PaccyC <abayisengaaimepacifique@gmail.com>
2024-08-23 15:51:38 +0200

    feat: features on the profile page

--- src/_root/pages/Profile.tsx
+++ src/_root/pages/Profile.tsx
@@ -1,8 +1,111 @@
+import { Button } from "../../components/ui/button";
+import { useUserContext } from "../../context/AuthContext";
 
 const Profile = () => {
-  return (
-    <div>Profile</div>
-  )
-}
+  const { user } = useUserContext();
 
-export default Profile
+  return (
+    <div className="flex flex-1">
+      <div className="common-container">
+        <div className="w-full flex justify-start items-center gap-6 max-w-5xl">
+          {/* Profile Image */}
+          <img
+            src="/assets/images/profile.png"
+            alt="user-profile-image"
+            height={150}
+            width={150}
+            className="rounded-full object-cover"
+          />
+
+          <div className="flex flex-col py-6">
+            <div className="flex gap-6 items-center">
+              <h2 className="md:h1-semibold h2-bold">Lewis Hamilton</h2>
+              <Button className="bg-dark-3 py-3 px-5 text-light-2 font-semibold text-lg flex items-center">
+                <img
+                  src="/assets/icons/edit-yellow.svg"
+                  width={15}
+                  height={15}
+                  alt="edit-icon"
+                  className="mr-2 fill-current text-yellow-400"
+                />
+                Edit Profile
+              </Button>
+            </div>
+            <p className="text-xl text-light-3">@Lewishamilton</p>
+            <div className="flex w-[60%] justify-between mt-3">
+              <div className="flex flex-col gap-1">
+                <h1 className="text-primary-500 font-medium text-lg">273</h1>
+                <p>Posts</p>
+              </div>
+              <div className="flex flex-col gap-1">
+                <h1 className="text-primary-500 font-medium text-lg">147</h1>
+                <p>Followers</p>
+              </div>
+              <div className="flex flex-col gap-1">
+                <h1 className="text-primary-500 font-medium text-lg">151</h1>
+                <p>Following</p>
+              </div>
+            </div>
+            <div className="mt-5">
+              <p className="flex items-center space-x-2">
+                <span>🌿</span>
+                <span>Capturing the essence of nature through my lens</span>
+              </p>
+              <p className="flex items-center space-x-2 mt-2">
+                <span>✨</span>
+                <span>
+                  "In every walk with nature, one receives far more than he
+                  seeks." - John Muir
+                </span>
+              </p>
+            </div>
+            <div className=" mt-5 flex gap-8 ">
+              <div className=" flex flex-col gap-2">
+                  <img 
+                  className="  rounded-full border-[3px] border-light-3"
+                  src="/assets/images/profile.png" 
+                  alt="" 
+                  width={72}
+                  height={72}
+                  />
+                  <p className="text-center text-sm font-semibold">BTS</p>
+               </div>
+               <div className=" flex flex-col gap-2">
+                <img 
+                className="  rounded-full border-[3px] border-light-3"
+                src="/assets/images/profile.png" 
+                alt="" 
+                width={72}
+                height={72}
+                />
+                <p className="text-center text-sm font-semibold">Frisbee</p>
+               </div>
+               <div className=" flex flex-col gap-2">
+                <img 
+                className="  rounded-full border-[3px] border-light-3"
+                src="/assets/images/profile.png" 
+                alt="" 
+                width={72}
+                height={72}
+                />
+                <p className="text-center text-sm font-semibold">Nature</p>
+               </div>
+               <div className=" flex flex-col gap-2 ">
+                <img 
+                className="rounded-full border-[3px] border-light-3"
+                src="/assets/images/profile.png" 
+                alt="" 
+                width={72}
+                height={72}
+                />
+                <p className=" text-center text-sm font-semibold">Outdoor</p>
+              </div> 
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
