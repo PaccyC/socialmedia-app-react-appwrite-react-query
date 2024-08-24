61ba7d2dd73550682e05c9bfbbe80bb42d355f17
PaccyC <abayisengaaimepacifique@gmail.com>
2024-08-24 09:37:45 +0200

    feat: Created EditProfileSidebarPosts component

--- src/components/ui/shared/EditProfileSIdebarPosts.tsx
+++ src/components/ui/shared/EditProfileSIdebarPosts.tsx
@@ -1,7 +1,7 @@
 
 const EditProfileSIdebarPosts = () => {
   return (
-    <div className=" grid grid-cols-1 gap-4 ">
+    <div className=" grid grid-cols-1 gap-4">
       <div className=" relative min-w-[330px] h-[315px] bg-slate-600 text-black rounded-2xl">
       EditProfileSIdebarPosts
       <div className=" absolute top-0 right-0 p-5">
